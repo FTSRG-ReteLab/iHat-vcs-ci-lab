0ea4b2cac669b871089ef19178d335f67d4a3800
Daniel Gal <daniel.gal@sch.bme.hu>
2019-03-25 07:36:04 -0700

    Implemented new alarm feature

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,8 +22,15 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		if (speedLimit > 500 ||
+			speedLimit < 0 ||
+			speedLimit < controller.getReferenceSpeed()*0.5) {
+				user.setAlarmState(true);
+		}
+		
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
+		
 	}
 
 }
