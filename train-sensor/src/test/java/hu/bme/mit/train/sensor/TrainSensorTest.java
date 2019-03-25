31b4b8db82a75eb95057a80afd2cf93ed9d50aa4
Daniel Gal <daniel.gal@sch.bme.hu>
2019-03-25 08:07:34 -0700

    Test high speed

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,38 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensorImpl sensor;
+
+    TrainUser mockUser;
+    TrainController mockController;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        mockController = mock(TrainController.class);
+        mockUser = mock(TrainUser.class);
+
+        sensor = new TrainSensorImpl(mockController, mockUser);
     }
 
+    // 1: 600 -> alarm
+    // 2: -20 -> alarm
+    // 3: 20 -> no alarm
+    // 4: mock controller, return ref. speed 50, set speedlimit 20
+
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void TestSetHighSpeedLimit() {
+        sensor.overrideSpeedLimit(600);
+        Assert.assertEquals(sensor.getSpeedLimit(), 600);
+        verify(mockUser, times(1)).setAlarmState(true);
     }
+
+
 }
